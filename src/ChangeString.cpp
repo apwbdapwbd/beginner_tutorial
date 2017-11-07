35987604d7172165a87fa0b60a4d5f857787c009
MichaelKam <mkam@terpmail.umd.edu>
2017-11-07 11:31:16 -0800

    1. Added two image in rqtScreenShot directory. 2. Used five logging levels in talker node. 3.Added doxygen message in talker and listener node

--- src/ChangeString.cpp
+++ src/ChangeString.cpp
@@ -1,29 +1,31 @@
+// "Copyright [2017] <Michael Kam>"
+/** @file ChangeString.cpp
+ *  @brief ChangeString.cpp is a node that serves as a client to request a ChangeString service.
+ *
+ *  @author Michael Kam (michael081906)
+ *  @bug No known bugs.
+ *  @copyright GNU Public License.
+ */
 #include "ros/ros.h"
 #include "beginner_tutorials/ChangeString.h"
 #include <cstdlib>
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   ros::init(argc, argv, "ChangeString");
-  if (argc != 2)
-  {
-    ROS_INFO("usage: changeString X");
+  if (argc != 2) {
+    ROS_ERROR("usage: changeString X");
     return 1;
   }
-
   ros::NodeHandle n;
-  ros::ServiceClient client = n.serviceClient<beginner_tutorials::ChangeString>("ChangeString");
+  ros::ServiceClient client = n.serviceClient < beginner_tutorials::ChangeString
+      > ("ChangeString");
   beginner_tutorials::ChangeString srv;
   srv.request.num = atoll(argv[1]);
-  if (client.call(srv))
-  {
+  if (client.call(srv)) {
     ROS_INFO("%s", srv.response.what.c_str());
-  }
-  else
-  {
-    ROS_ERROR("Failed to call service ChangeString");
+  } else {
+    ROS_FATAL("Failed to call service ChangeString");
     return 1;
   }
-
   return 0;
 }
--- src/listener.cpp
+++ src/listener.cpp
@@ -9,16 +9,19 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 
+/**@brief This function is a callback function to output a msg content
+ * @param[in] msg is a message type
+ * @return none
+ */
 void chatterCallback(const std_msgs::String::ConstPtr& msg) {
   ROS_INFO("I heard: [%s]", msg->data.c_str());
 }
 
 int main(int argc, char **argv) {
-
   ros::init(argc, argv, "listener");
   ros::NodeHandle n;
   ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
+  ROS_DEBUG("Start subscribe to topic chatter");
   ros::spin();
-
   return 0;
 }
